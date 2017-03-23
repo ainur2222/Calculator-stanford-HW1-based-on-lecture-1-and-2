eb15ee377c223d6428dab57e63d6853bcd75b0ce
Айнур Самигуллин <ainur2222@gmail.com>
2017-03-23 12:15:09 +0300

    Initial Commit

---
+++ Calculator stanford/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator stanford
+//
+//  Created by Айнур Самигуллин on 23/03/2017.
+//  Copyright © 2017 Айнур Самигуллин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
