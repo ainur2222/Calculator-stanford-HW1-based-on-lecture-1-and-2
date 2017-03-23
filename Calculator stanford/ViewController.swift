dd5d6c77ede0de7f0fc39f933dc255fde1a6a17b
Айнур Самигуллин <ainur2222@gmail.com>
2017-03-23 19:16:55 +0300

    Первая лекция

--- Calculator stanford/ViewController.swift
+++ Calculator stanford/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet weak var display: UILabel!  // explicitly unwrapped optional, теперь можно не писать display!, а можно писать только display
+    
+    
+    
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        print("\(digit) was pressed")
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyOnDisplay = display.text!
+            display.text = textCurrentlyOnDisplay + digit
+        } else {
+            display?.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    
+    
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false  // убираем возможность писать цифры после того как нажали на pi
+        
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "pi":
+                //display.text = String(Double.pi)
+                displayValue = Double.pi
+            case "√":
+//                let operant = Double(display.text!)!
+//                display.text = String(sqrt(operant))
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
+ 
