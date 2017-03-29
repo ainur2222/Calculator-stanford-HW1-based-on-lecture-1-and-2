c42fac1e45beb4468e9ebbf55985e99012b90e8c
Айнур Самигуллин <ainur2222@gmail.com>
2017-03-29 23:17:13 +0300

    первый коммит

--- Calculator stanford/CalculatorBrain - modelInMVC.swift
+++ Calculator stanford/CalculatorBrain - modelInMVC.swift
@@ -7,3 +7,135 @@
 //
 
 import Foundation
+
+
+
+struct CalculatorBrain {
+    
+    
+    
+    var resultIsPending = false
+    
+    
+    private var accumulator: Double? // опциональный тип тк в начале программы аккумулор пуст => nill =) опциональный тип
+    
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperaion((Double, Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String,Operation> = [  //set Dictionary
+        "pi" : Operation.constant(Double.pi),  // Double.pi,
+        "e"  : Operation.constant(M_E),        // M_E
+        "√"  : Operation.unaryOperation(sqrt),       // sqrt(accumulator)
+        "cos": Operation.unaryOperation(cos),        // cos
+        "±"  : Operation.unaryOperation({  -$0    }),
+        "×"  : Operation.binaryOperaion({  $0 * $1  }),  // CLOSURES почитать !!!
+        "÷"  : Operation.binaryOperaion({  $0 / $1  }),
+        "+"  : Operation.binaryOperaion({  $0 + $1  }),
+        "-"  : Operation.binaryOperaion({  $0 - $1  }),
+        "="  : Operation.equals,
+        "x^2": Operation.unaryOperation({ $0 * $0 }),
+        "x^5": Operation.unaryOperation({ $0 * $0 * $0 * $0 * $0 }),
+//        "C"  : Operation.constant(0),
+        "sin": Operation.unaryOperation(sin)
+    ]
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation5 = operations[symbol] {
+            switch operation5 {
+            case .constant(let Value):
+                accumulator = Value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperaion(let function):
+                if accumulator != nil {
+                pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            }
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perforn(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+    }
+    
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {   // делаем еще один стракт для решения проблемы с "бо он очень часто будет not set, как в примере "5 *" - не установлен, а потом "5 * 3" и вот теперь только он не nill"
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+        
+        func perforn(with secondOperand:Double) -> Double {
+            return function(firstOperand, secondOperand)
+
+        }
+    }
+    
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    var result: Double? {
+        // ставим опциональный ? т.к. мы не можем поставить ! около accumulator ибо он очень часто будет not set, как в примере "5 *" - не установлен, а потом "5 * 3" и вот теперь только он не nill
+        get{
+            return accumulator
+        }
+    }
+    
+    
+    mutating func returnToInitialState() {
+        accumulator = nil
+        //pendingBinaryOperation = nil
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+//func changeSign(operand:Double) -> Double {
+//    return -operand
+//}
+//func multiply
+// делаем не class, а struct так как нам не нужно будет создавать подклассы  и для того, чтобы научится пользоваться struct.  Struct is value type, Class is reference type
+
+
+
+
+
+
+
+// that is a mess)) we cant describe every case? so use swifts language good stuff
+//        switch symbol {
+//        case "pi":
+//            //display.text = String(Double.pi)
+//            accumulator = Double.pi
+//        case "√":
+//            //let operant = Double(display.text!)!
+//            //display.text = String(sqrt(operant))
+//            if let operand = accumulator {
+//                accumulator = sqrt(operand)
+//            }
+//
+//        default:
+//            break
+//        }
--- Calculator stanford/ViewController.swift
+++ Calculator stanford/ViewController.swift
@@ -13,23 +13,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!  // explicitly unwrapped optional, теперь можно не писать display!, а можно писать только display
     
-    
-    
-    
     var userIsInTheMiddleOfTyping = false
-    
+    var countDots = 0  //считаем кол-во точек в числе. для ввода корректного Double
     
     @IBAction func touchDigit(_ sender: UIButton) {
-        let digit = sender.currentTitle!
-        print("\(digit) was pressed")
-        if userIsInTheMiddleOfTyping {
-            let textCurrentlyOnDisplay = display.text!
-            display.text = textCurrentlyOnDisplay + digit
-        } else {
-            display?.text = digit
-            userIsInTheMiddleOfTyping = true
+        if sender.currentTitle == "." {
+            countDots = countDots + 1
         }
+        if countDots < 2 {
+            let digit = sender.currentTitle!
+            print("\(digit) was pressed")
+            if userIsInTheMiddleOfTyping {
+                let textCurrentlyOnDisplay = display.text!
+                display.text = textCurrentlyOnDisplay + digit
+            } else {
+                display?.text = digit
+                userIsInTheMiddleOfTyping = true
+            }
+        } else {
+            brain.returnToInitialState()
+            display.text = "Illegal number. Press C"
+            
+        }
+        
+        
+        
+        
+        
+        
     }
+    
     
     var displayValue: Double {
         get {
@@ -40,35 +53,54 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    private var brain = CalculatorBrain()   // brain это наш обьект нашей единственной главной структуры в файле modelInMVC
     
     
     @IBAction func performOperation(_ sender: UIButton) {
-        userIsInTheMiddleOfTyping = false  // убираем возможность писать цифры после того как нажали на pi
-        
-        if let mathematicalSymbol = sender.currentTitle {
-            switch mathematicalSymbol {
-            case "pi":
-                //display.text = String(Double.pi)
-                displayValue = Double.pi
-            case "√":
-//                let operant = Double(display.text!)!
-//                display.text = String(sqrt(operant))
-                displayValue = sqrt(displayValue)
-            default:
-                break
+        if display.text != "Illegal number. Press C"{
+            if userIsInTheMiddleOfTyping {
+                brain.setOperand(displayValue)
+                userIsInTheMiddleOfTyping = false
             }
-            
-            
+            // убираем возможность дописывать цифры после того как нажали на pi
+            if let mathematicalSymbol = sender.currentTitle {
+                brain.performOperation(mathematicalSymbol)
+            }
+            if let result = brain.result {   // if brain.result is determined
+                displayValue = result
+            }
+        }
+    }
+    
+    
+    //    @IBAction func CButton(_ sender: UIButton) {
+    //        display.text = "0"
+    //        brain.returnToInitialState()
+    //        if userIsInTheMiddleOfTyping {
+    //            userIsInTheMiddleOfTyping = false
+    //        } else {
+    //        }
+    //    }
+    
+    @IBAction func cButton(_ sender: UIButton) {
+        display.text = "0"
+        countDots = 0
+        brain.returnToInitialState()
+        if userIsInTheMiddleOfTyping {
+            userIsInTheMiddleOfTyping = false
         }
         
     }
     
     
     
-    
-    
-    
 }
 
- 
+
+
+
+
+
+
+
+
